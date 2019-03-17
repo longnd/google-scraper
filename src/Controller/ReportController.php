f8749515dda17ddd6936ad6edc6ab641d9bb62d5
Long Nguyen <nguyenduclong@msn.com>
2019-03-17 14:44:08 +0700

    show detail report

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -3,18 +3,47 @@
 namespace App\Controller;
 
 use App\Repository\ScrapingRequestRepository;
+use App\Repository\ScrapingResultRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ReportController extends AbstractController
 {
+    private $scrapingRequestRepo;
+    private $scrapingResultRepo;
+    private $translator;
+
+    public function __construct(
+        ScrapingRequestRepository $scrapingRequestRepo,
+        ScrapingResultRepository $scrapingResultRepo,
+        TranslatorInterface $translator
+    ) {
+        $this->scrapingRequestRepo = $scrapingRequestRepo;
+        $this->scrapingResultRepo = $scrapingResultRepo;
+        $this->translator = $translator;
+    }
+
     /**
      * @Route("/reports", name="reports")
      */
-    public function index(ScrapingRequestRepository $scrapingRequestRepo)
+    public function index()
     {
-        $scrapingRequests = $scrapingRequestRepo->getAllScrapingRequest();
+        $scrapingRequests = $this->scrapingRequestRepo->getAllScrapingRequest();
 
         return $this->render('report/general.html.twig', ['requests' => $scrapingRequests]);
+    }
+
+    /**
+     * @Route("/reports/{id}", name="detailReport")
+     */
+    public function view($id)
+    {
+        if (!$scrapingRequest = $this->scrapingRequestRepo->find($id)) {
+            throw $this->createNotFoundException($this->translator->trans('error.report_not_found'));
+        }
+        $scrapingResults = $this->scrapingResultRepo->getResults($scrapingRequest);
+
+        return $this->render('report/detail.html.twig', ['results' => $scrapingResults]);
     }
 }
