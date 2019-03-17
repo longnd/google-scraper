c7f7a2d7cccf3adf89cb1bd7e5463fc7ebcfa6ac
Long Nguyen <nguyenduclong@msn.com>
2019-03-17 16:49:28 +0700

    allow to download crawled HTML file of the search result

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ScrapingRequestRepository;
 use App\Repository\ScrapingResultRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -45,5 +46,28 @@ class ReportController extends AbstractController
         $scrapingResults = $this->scrapingResultRepo->getResults($scrapingRequest);
 
         return $this->render('report/detail.html.twig', ['results' => $scrapingResults]);
+    }
+
+    /**
+     * @Route("/reports/result/{id}/html", name="searchResultHtml")
+     */
+    public function showCrawledHtml($id)
+    {
+        if (!$scrapingResult = $this->scrapingResultRepo->find($id)) {
+            throw $this->createNotFoundException($this->translator->trans('error.result_not_found'));
+        }
+
+        $response = new StreamedResponse();
+        $fileName = sprintf('%s.html', str_replace(' ', '_', trim($scrapingResult->getKeyword())));
+        $response->setCallback(function () use ($scrapingResult) {
+            $handle = fopen('php://output', 'w+');
+            fwrite($handle, $scrapingResult->getHtml());
+            fclose($handle);
+        });
+
+        $response->headers->set('Content-Type', 'text/html; charset=utf-8');
+        $response->headers->set('Content-Disposition', "attachment; filename=$fileName");
+
+        return $response;
     }
 }
